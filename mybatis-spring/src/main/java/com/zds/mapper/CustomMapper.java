b66b2299cc5cd162b9ac9c6ec7d802220613abb9
zhongds01 <zhongds01@163.com>
2021-08-21 23:34:38 +0800

    mybatis-spring事务管理简单测试

--- mybatis-spring/src/main/java/com/zds/mapper/CustomMapper.java
+++ mybatis-spring/src/main/java/com/zds/mapper/CustomMapper.java
@@ -1,7 +1,9 @@
 package com.zds.mapper;
 
 import com.zds.pojo.Custom;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -9,4 +11,8 @@ public interface CustomMapper {
     List<Custom> selectOneCustomById(@Param("id") Long id);
 
     int insertCustom(@Param("custom") Custom custom);
+
+    int updateCustomById(@Param("custom") Custom custom);
+
+    List<Custom> selectCustom();
 }
--- mybatis-spring/src/test/java/com/zds/AppTest.java
+++ mybatis-spring/src/test/java/com/zds/AppTest.java
@@ -1,15 +1,24 @@
 package com.zds;
 
 import com.zds.mapper.CustomMapper;
-import javafx.application.Application;
+import com.zds.pojo.Custom;
+import com.zds.service.CustomService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * Unit test for CustomMapper.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:application.xml")
 public class AppTest {
+    @Autowired
+    private CustomService customService;
     /**
      * testSelectCustomById
      */
@@ -18,5 +27,17 @@ public class AppTest {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:application.xml");
         CustomMapper bean = applicationContext.getBean(CustomMapper.class);
         bean.selectOneCustomById(1415300753928499202L).forEach(System.out::println);
+    }
+
+    @Test
+    public void testTransactionalInsert() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:application.xml");
+        CustomService customServiceImpl = applicationContext.getBean("customServiceImpl", CustomService.class);
+        customServiceImpl.insertCustom(Custom.builder().id(1415300753928499206L).customName("zhongdongsheng").customPwd("123456").customSex("男").customTel("13260906627").customEmail("zhongds01@163.com").customAddress("Nanjing").status("U").build());
+    }
+
+    @Test
+    public void testUpdate(){
+        customService.updateCustomById(Custom.builder().id(1415300753928499206L).customName("shengdongzhong").customPwd("654321").customSex("男").customTel("13260906627").customEmail("zhongds01@163.com").customAddress("Nanjing").status("E").build());
     }
 }
