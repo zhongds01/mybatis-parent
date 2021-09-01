89174271846ebf740e661ef285cabb50520ce1bc
zhongds01 <zhongds01@163.com>
2021-09-01 15:26:42 +0800

    ICustomerService接口重命名

--- mybatis-plus/src/main/java/com/zds/service/ICustomService.java
+++
@@ -1,7 +0,0 @@
-package com.zds.service;
-
-import com.zds.base.IBaseService;
-import com.zds.entity.Customer;
-
-public interface ICustomService extends IBaseService<Customer> {
-}
---
+++ mybatis-plus/src/main/java/com/zds/service/ICustomerService.java
@@ -0,0 +1,10 @@
+package com.zds.service;
+
+import com.zds.base.IBaseService;
+import com.zds.entity.Customer;
+
+/**
+ * @author zhongdongsheng
+ * @datetime 2021/9/1 09:50
+ */
+public interface ICustomerService extends IBaseService<Customer> {}
